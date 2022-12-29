ad13d66107823d36b5f461d15b70f925f3513481
Changhwan Kim <kimch061279@gmail.com>
2022-12-29 15:14:31 +0000

    Rebase codes

--- bap_crawler.py
+++ bap_crawler.py
@@ -1,15 +1,16 @@
 # NSU 학식 크롤링 코드 #
-from bs4 import BeautifulSoup
-from urllib import parse
-import requests, json
 
-def NSU_Meal():
-    timeout = 5
-    url = parse.urlparse('https://nsu.ac.kr/api/user/board/getBoardContentSummaryList')
-    headers = {'Content-Type' : 'application/x-www-form-urlencoded;'}
-    data = {'boardIdList=468&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4'}
-    #465 1층, 466 2층, 467 3층, 468 채움
-    res = requests.post(url, data=data, headers=headers, timeout=timeout)
-    print(res)
+import requests
 
-NSU_Meal()
+def NSU_MEAL():
+    strUrl = "https://nsu.ac.kr/api/user/board/getBoardContentSummaryList"
+
+    mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=468&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4").json()
+    mealResponse = dict(mealResponse)
+
+    mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][0]
+
+    for mealData in mealList.items():
+        print(f"Field : {mealData[0]}\n{mealData[1]}")
+
+NSU_MEAL()
