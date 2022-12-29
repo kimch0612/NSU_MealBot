04fec18e0c0e64ab8997f69ee3bdc266748e1ab2
Changhwan Kim <kimch061279@gmail.com>
2022-12-29 14:34:50 +0000

    Base features Coding

--- bap_crawler.py
+++ bap_crawler.py
@@ -1,3 +1,15 @@
 # NSU 학식 크롤링 코드 #
+from bs4 import BeautifulSoup
+from urllib import parse
+import requests, json
 
-import os
+def NSU_Meal():
+    timeout = 5
+    url = parse.urlparse('https://nsu.ac.kr/api/user/board/getBoardContentSummaryList')
+    headers = {'Content-Type' : 'application/x-www-form-urlencoded;'}
+    data = {'boardIdList=468&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4'}
+    #465 1층, 466 2층, 467 3층, 468 채움
+    res = requests.post(url, data=data, headers=headers, timeout=timeout)
+    print(res)
+
+NSU_Meal()
