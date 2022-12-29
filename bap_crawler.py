068e4c382863728c2d0aeebf3e4c0def9380eb2a
Changhwan Kim <kimch061279@gmail.com>
2022-12-29 23:52:46 +0000

    위치설정 추가

--- bap_crawler.py
+++ bap_crawler.py
@@ -1,11 +1,18 @@
 # NSU 학식 크롤링 코드 #
-
 import requests
 
 def NSU_MEAL():
-    strUrl = "https://nsu.ac.kr/api/user/board/getBoardContentSummaryList"
+    if NSU_MSG == "1층":
+        location = 465
+    elif NSU_MSG == "2층":
+        location = 466
+    elif NSU_MSG == "3층":
+        location = 467
+    elif NSU_MSG == "채움":
+        location = 468
 
-    mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=468&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4").json()
+    strUrl = "https://nsu.ac.kr/api/user/board/getBoardContentSummaryList"
+    mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=%d&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4" % location).json()
     mealResponse = dict(mealResponse)
 
     mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][0]
@@ -13,4 +20,5 @@ def NSU_MEAL():
     for mealData in mealList.items():
         print(f"Field : {mealData[0]}\n{mealData[1]}")
 
+NSU_MSG = input("위치를 입력하세요: ")
 NSU_MEAL()
