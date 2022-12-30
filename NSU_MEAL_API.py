043ca174ce31ba609d10a8dba2d35fb552ee563d
Changhwan Kim <kimch061279@gmail.com>
2022-12-30 03:41:17 +0000

    Updates Codes

--- NSU_MEAL_API.py
+++ NSU_MEAL_API.py
@@ -3,28 +3,23 @@ import requests
 
 def getReplyMessage():
     strResult = ""
-    global NSU_BAP
     if "남샤" in message:
         if "1층" in message:
-            NSU_BAP = 465
+            strResult = messageNSUMeal("465")
         elif "2층" in message:
-            NSU_BAP = 466
+            strResult = messageNSUMeal("466")
         elif "3층" in message:
-            NSU_BAP = 467
+            strResult = messageNSUMeal("467")
         elif "채움" in message:
-            NSU_BAP = 468
+            strResult = messageNSUMeal("468")
 
-def NSU_MEAL():
+def messageNSUMeal(NSU_BAP):
     strUrl = "https://nsu.ac.kr/api/user/board/getBoardContentSummaryList"
     mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=%d&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4" % int(NSU_BAP)).json()
     mealResponse = dict(mealResponse)
     mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][0]
 
-    mealList['//월요일//'] = mealList['field1']
-    mealList['//화요일//'] = mealList['field2']
-    mealList['//수요일//'] = mealList['field3']
-    mealList['//목요일//'] = mealList['field4']
-    mealList['//금요일//'] = mealList['field5']
+    mealList['// 월요일 //'], mealList['// 화요일 //'], mealList['// 수요일 //'], mealList['// 목요일 //'], mealList['// 금요일 //'], = mealList['field1'], mealList['field2'], mealList['field3'], mealList['field4'], mealList['field5']
     del(mealList['corner'], mealList['field1'], mealList['field2'], mealList['field3'], mealList['field4'], mealList['field5'])
 
     for mealData in mealList.items():
@@ -32,4 +27,4 @@ def NSU_MEAL():
 
 message = input("위치를 입력하세요: ")
 getReplyMessage()
-NSU_MEAL()
+messageNSUMeal()
