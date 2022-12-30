c14784534c37b5895daf11740fcf0f19297f764b
Changhwan Kim <kimch061279@gmail.com>
2022-12-30 01:14:08 +0000

    코드 개선

--- NSU_MEAL_API.py
+++ NSU_MEAL_API.py
@@ -1,20 +1,23 @@
 # NSU 학식 크롤링 코드 #
 import requests
 
+def getReplyMessage():
+    strResult = ""
+    global NSU_BAP
+    if "남샤" in message:
+        if "1층" in message:
+            NSU_BAP = 465
+        elif "2층" in message:
+            NSU_BAP = 466
+        elif "3층" in message:
+            NSU_BAP = 467
+        elif "채움" in message:
+            NSU_BAP = 468
+
 def NSU_MEAL():
-    if NSU_MSG == "1층":
-        location = 465
-    elif NSU_MSG == "2층":
-        location = 466
-    elif NSU_MSG == "3층":
-        location = 467
-    elif NSU_MSG == "채움":
-        location = 468
-
     strUrl = "https://nsu.ac.kr/api/user/board/getBoardContentSummaryList"
-    mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=%d&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4" % location).json()
+    mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=%d&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4" % int(NSU_BAP)).json()
     mealResponse = dict(mealResponse)
-
     mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][0]
 
     mealList['//월요일//'] = mealList['field1']
@@ -25,7 +28,8 @@ def NSU_MEAL():
     del(mealList['corner'], mealList['field1'], mealList['field2'], mealList['field3'], mealList['field4'], mealList['field5'])
 
     for mealData in mealList.items():
-        print(f"{mealData[0]}\n{mealData[1]}")
+        print(f"{mealData[0]}\n{mealData[1]}\n")
 
-NSU_MSG = input("위치를 입력하세요: ")
+message = input("위치를 입력하세요: ")
+getReplyMessage()
 NSU_MEAL()
