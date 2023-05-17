30a32010fd09cf78c15a80cc7ad5173f1e45af22
Changhwan Kim <kimch061279@gmail.com>
2023-05-17 10:23:06 +0900

    Update NSU_MEAL_API.py
    1. 카페테리움 조식 중식 기능 추가
    2. 불러온 학식 식단의 날짜 출력 기능 추가

--- NSU_MEAL_API.py
+++ NSU_MEAL_API.py
@@ -11,20 +11,28 @@ def getReplyMessage():
         elif "3층" in message:
             strResult = messageNSUMeal("467")
         elif "카페" in message:
-            strResult = messageNSUMeal("468")
+            if "조식" in message:
+                strResult = messageNSUMeal("468", "0")
+            elif "중식" in message:
+                strResult = messageNSUMeal("468", "1")
 
-def messageNSUMeal(NSU_BAP):
+def messageNSUMeal(NSU_BAP, Food_list):
     strMessage = ""
     strUrl = "https://nsu.ac.kr/api/user/board/getBoardContentSummaryList"
     mealResponse = requests.post(strUrl, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data="boardIdList=%d&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4" % int(NSU_BAP)).json()
     mealResponse = dict(mealResponse)
-    mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][0]
+    mealDate = mealResponse["body"]["list"][0]["title"]
+    if food_list == 0:
+        mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][0]
+    elif food_list == 1:
+        mealList = mealResponse["body"]["list"][0]["properties"]["food_list"][1]
 
     mealList['// 월요일 //'], mealList['// 화요일 //'], mealList['// 수요일 //'], mealList['// 목요일 //'], mealList['// 금요일 //'], = mealList['field1'], mealList['field2'], mealList['field3'], mealList['field4'], mealList['field5']
     del(mealList['corner'], mealList['field1'], mealList['field2'], mealList['field3'], mealList['field4'], mealList['field5'])
 
     for mealData in mealList.items():
         strMessage += (f"{mealData[0]}\n{mealData[1]}\n")
+    strMessage = mealDate + "\n\n" + strMessage
     return strMessage
 
 getReplyMessage()
